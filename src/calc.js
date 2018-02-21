9565c1d91b5525849cf0892aac9cdfbe3acaca1c
trgomes <trtg10@gmail.com>
2018-02-21 01:54:10 -0300

    update

--- src/calc.js
+++ src/calc.js
@@ -3,9 +3,4 @@ const sub = (num1, num2) => num1 - num2;
 const mult = (num1, num2) => num1 * num2;
 const div = (num1, num2) => (num2 === 0) ? 'Não é possível' : num1 / num2;
 
-export {
-  sum,
-  sub,
-  mult,
-  div
-}
+export { sum, sub, mult, div };
